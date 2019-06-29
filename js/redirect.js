3fa25db76640a4ad822517b5a9b6e55370754060
ZandroDadullaJr <33176442+ZandroDadullaJr@users.noreply.github.com>
2019-06-29 03:06:23 -0400

    Add files via upload

---
+++ js/redirect.js
@@ -0,0 +1,21 @@
+function qsee1() {
+    var url = "http://172.16.2.1:85";
+    window.open(url);
+}
+
+function qsee2() {
+    var url = "http://172.16.2.2:85";
+    window.open(url);
+}
+
+function owa() {
+    var url = "https://owa.asiapremieronesource.com/ecp";
+    window.open(url);
+}
+
+https://mars:8445/Reporting/login/login.php
+
+function symantec() {
+    var url = "https://mars:8445/Reporting/login/login.php";
+    window.open(url);
+}
